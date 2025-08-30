b5faea669ea113255f9878cc2c31637a6dac2373
Ajay reddy <ajayreddychinthala110125@gmail.com>
2025-08-31 00:05:33 +0530

    db

--- includes/db/db.php
+++ includes/db/db.php
@@ -1,8 +1,8 @@
 <?php 
 
-$dsn = 'mysql:host=localhost; dbname=vktsr';
-$user = 'root';
-$password = '';
+$dsn = 'mysql:host=localhost; dbname=agri_invest_harvest';
+$user = 'agri_invest_user';
+$password = 'GK%Yb0vW@0tNBVe';
 
 
 try{
