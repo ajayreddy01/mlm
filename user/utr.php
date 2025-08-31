3917ece13c46a1e7ef60014572c4f737000433bc
Ajay reddy <ajayreddychinthala110125@gmail.com>
2025-09-01 02:54:18 +0530

    Update utr.php

--- user/utr.php
+++ user/utr.php
@@ -44,8 +44,8 @@ if (isset($_POST['submit'])) {
     ];
     
     $wallet->deposit($_SESSION['userid'], $amount, $data);
-    header('Location: https://agriinvestharvest.com/user/dashbaord.php');
-    exit;
+    //header('Location: https://agriinvestharvest.com/user/dashbaord.php');
+    //exit;
   } else {
     echo "Please provide both UTR Number and an image.";
   }
