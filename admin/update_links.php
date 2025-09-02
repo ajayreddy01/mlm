90f27ed0f391bdb9c46f7964f6b30b3e880b5607
Ajay reddy <ajayreddychinthala110125@gmail.com>
2025-09-03 02:41:31 +0530

    Update update_links.php

--- admin/update_links.php
+++ admin/update_links.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         "    'telegram' => '" . addslashes($telegram) . "',\n" .
         "];\n";
 
-    file_put_contents(__DIR__ . '/config/social_links.php', $configContent);
+    file_put_contents(__DIR__ . '../includes/social.php', $configContent);
 
     header("Location: dashboard.php?updated=1");
     exit;
